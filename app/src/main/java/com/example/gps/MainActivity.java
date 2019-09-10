c84e33008fb8bec55289b2e090a032d6cfc83dea
AlyssonMiguel <amgcorrea95@gmail.com>
2019-09-09 21:52:30 -0300

    GPS

---
+++ app/src/main/java/com/example/gps/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.gps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.security.PublicKey;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocation;
+    private static final int CODIGO_GPS = 1000;
+    private TextView textView;
+    private Location minhaLoc = new Location("Minha Localização");
+    private Location catedralLoc = new Location("Localização da Catedral");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textViewDistancia);
+    }
+
+    public void mostrarDistancia(View v){
+
+
+        //Verifica se a permissao não foi concedida
+
+        if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED);
+
+        // Solicita o uso do GPS para o usuário
+
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION
+        }, CODIGO_GPS);
+
+        fusedLocation = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocation.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        minhaLoc.setLatitude(location.getLatitude());
+                        minhaLoc.setLongitude(location.getLongitude());
+                        calculaDistancia();
+                    }
+                });
+    }
+    public void calculaDistancia(){
+        catedralLoc.setLatitude(-22.018229);
+        catedralLoc.setLongitude(-47.8934227);
+
+        Float distancia = minhaLoc.distanceTo(catedralLoc)/1000;
+        textView.setText(String.format("%.2f",distancia) + "  Km");
+    }
+
+
+}
